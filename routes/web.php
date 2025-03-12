ba7761225912c702f3d201cbe21372cdd4da5d07
surya1810 <surya.10rpl1.2019@gmail.com>
2025-03-12 15:02:36 +0700

    update login

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // Maintenance
-    return view('maintenance');
-    // return redirect('dashboard');
+    return redirect()->route('dashboard');
 });
 
 Auth::routes();
